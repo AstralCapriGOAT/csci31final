b7cf1f777973c374ffd99788b30d5c6dcac33717
Brendon C <bcrockett001@student.butte.edu>
2023-11-09 14:33:18 -0800

    first supabase test

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,27 @@
 import Navbar from './components/Navbar'
-export default function Home() {
+import { createClient } from '@supabase/supabase-js'
+
+// Create a single supabase client for interacting with your database
+const supabase = createClient('https://vaqdcjrivplagaprhqbr.supabase.co', process.env.SUPABASE_SECRET)
+
+export const revalidate = 15
+export default async function Home() {
+  let { data: cards, error } = await supabase.from('cards').select('id')
+
+  console.log('cards: ', cards)
+
   return (
     <div>
       <Navbar />
-      <p>Home</p>
+      <div className="m-12">
+        <PageTitle title="Home" />
+        <PageContent />
+        <div className="flex gap-6 w-full my-6 flex-wrap">
+          {cards.map((card) => (
+            <Card title={card.title} subtitle={card.subtitle} description={card.description} />
+          ))}
+        </div>
+      </div>
     </div>
   )
 }
